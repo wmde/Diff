1c80b47100a368b8c6342c0b41938a905b2cd248
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-12-28 17:13:11 +0100

    Avoid full qualified class names

--- src/Patcher/PatcherException.php
+++ src/Patcher/PatcherException.php
@@ -2,6 +2,14 @@
 
 namespace Diff\Patcher;
 
-class PatcherException extends \RuntimeException {
+use RuntimeException;
+
+/**
+ * @since 0.4
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class PatcherException extends RuntimeException {
 
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,6 +14,8 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-$autoLoader = require __DIR__ . '/../vendor/autoload.php';
+$classLoader = require __DIR__ . '/../vendor/autoload.php';
 
-$autoLoader->addPsr4( 'Diff\\Tests\\', __DIR__ . '/phpunit/' );
+$classLoader->addPsr4( 'Diff\\Tests\\', __DIR__ . '/phpunit/' );
+
+unset( $classLoader );
