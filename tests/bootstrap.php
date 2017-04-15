979e05ae1557d2059e46f1985295539dfb8dbe71
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-04-16 01:39:54 +0200

    Added error settings to PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,6 +13,9 @@ if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
+error_reporting( -1 );
+ini_set( 'display_errors', '1' );
+
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
